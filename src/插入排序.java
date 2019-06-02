228df052ed1c6c3e5d6e0c8592e1afe24f3b110f
ck <ck04216417@gmail.com>
2019-06-02 16:44:42 +0800

    选择排序优化

---
+++ src/插入排序.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class 插入排序 {
+    public static void main(String[] args) {
+        int[] ck = {3, 9, 4, 5, 6, 7};
+        Sort(ck);
+        System.out.println(Arrays.toString(ck));
+    }
+
+    /*    static  void Sort(int[] nums){
+            int len =nums.length;
+            for(int i=1;i<len;i++){
+                for(int j=i;j>0;j--){
+                    if(nums[j]<nums[j-1]){
+                        int temp = nums[j-1];
+                        nums[j-1] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+        }*/
+    static void Sort(int[] nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            int temp = nums[i];
+            for (int j = i; j >0; j--) {
+                if (temp < nums[j - 1]) {
+                    nums[j] = nums[j-1];
+                } else {
+                    nums[j] = temp;
+                    break;
+                }
+            }
+        }
+    }
+}
